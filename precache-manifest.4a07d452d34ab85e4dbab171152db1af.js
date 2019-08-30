5b0a059bbe567b57cb41ac3dcf1370f29f07b27d
Vassily Krasnov <mykrasnov@gmail.com>
2019-08-30 23:10:52 +0300

    Updates

---
+++ precache-manifest.4a07d452d34ab85e4dbab171152db1af.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "afc51d95a7c3316a809bd8627ba8481a",
+    "url": "/lplates/index.html"
+  },
+  {
+    "revision": "52df93e0914ef12f1341",
+    "url": "/lplates/static/css/main.b95837b6.chunk.css"
+  },
+  {
+    "revision": "b36b62db27f90fdc31c7",
+    "url": "/lplates/static/js/2.fe5e35b9.chunk.js"
+  },
+  {
+    "revision": "52df93e0914ef12f1341",
+    "url": "/lplates/static/js/main.96bd2e39.chunk.js"
+  },
+  {
+    "revision": "2f9e84fd4d867fc754b5",
+    "url": "/lplates/static/js/runtime~main.49104f74.js"
+  }
+]);
